6779319545e6d7a25b419ca3a24ad04f073a1b80
Sasha Ritvinsky <ritsasha01@gmail.com>
2020-11-05 10:41:17 +0300

    Added account settings window

---
+++ Automation_of_accounting_of_MTZ_components/AccountSettings.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Automation_of_accounting_of_MTZ_components
+{
+    /// <summary>
+    /// Логика взаимодействия для AccountSettings.xaml
+    /// </summary>
+    public partial class AccountSettings : Window
+    {
+        public AccountSettings()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
